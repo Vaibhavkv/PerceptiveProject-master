e03d0ac66285b9bd0fd1939b695394bb057062cd
neilroy1998 <neilroy1998@gmail.com>
2019-04-11 20:14:16 +0530

    a lot 2

--- app/src/main/java/com/example/perceptive/homePageActivity.java
+++ app/src/main/java/com/example/perceptive/homePageActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -18,6 +19,17 @@ public class homePageActivity extends AppCompatActivity {
 
     public void search_bclk (View v) {
         Intent i = new Intent(this, searchActivity.class);
+        startActivity(i);
+    }
+
+    public void art_clk(View v) {
+        Intent i = new Intent(this, ListActivity.class);
+        i.putExtra("pgt", "1");
+        startActivity(i);
+    }
+    public void mov_clk(View v){
+        Intent i = new Intent(this, ListActivity.class);
+        i.putExtra("pgt", "0");
         startActivity(i);
     }
 }
