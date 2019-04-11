5786e4eccfd46ef2a05d7bd4de15fbe6acc2c0dd
neilroy1998 <neilroy1998@gmail.com>
2019-04-11 14:09:04 +0530

    add reg 1

--- app/src/main/java/com/example/perceptive/startPageActivity.java
+++ app/src/main/java/com/example/perceptive/startPageActivity.java
@@ -21,7 +21,7 @@ public class startPageActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         ab.hide();
 
-        /*ArrayList<user> userdatatemp = new ArrayList<user>();
+ /*       ArrayList<user> userdatatemp = new ArrayList<user>();
         ArrayList<Object> userdata = new ArrayList<Object>();
         userdatatemp.add(new user(1,"Tom","tom@gmail.com","tom",11111));
         userdatatemp.add(new user(2,"Jack","jack@gmail.com","jack",22222));
@@ -38,7 +38,9 @@ public class startPageActivity extends AppCompatActivity {
         Intent i = new Intent(startPageActivity.this, loginActivity.class);
         startActivity(i);
     }
-}
-class abc{
 
+    public void reg_clk(View v) {
+        Intent i = new Intent(startPageActivity.this, RegActivity.class);
+        startActivity(i);
+    }
 }
